1e6d665693aa359825d9545e81200ce00ee0ce1a
ribiliamin <bayo.biliamin15@kwasu.edu.ng>
2023-11-28 08:33:44 -0800

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
